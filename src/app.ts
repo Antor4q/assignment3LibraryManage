ac0a89232298102c047dfc868e195c3ab0f8d3f9
Antor4q <tariquelislam2015@gmail.com>
2025-09-12 12:45:14 +0600

    cors links changed

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,20 @@ import cors from 'cors';
 const app:Application = express()
 
 app.use(express.json())
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://athenaeum-lib.vercel.app","https://athenaeum-lib.vercel.app"]
-   })
-);
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://athenaeum-lib.vercel.app"
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 
 
